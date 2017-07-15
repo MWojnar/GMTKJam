bf3004b1e2a6061954236df3da4f4cca2e67794b
Michael Wojnar <michael.j.wojnar@gmail.com>
2017-07-14 19:44:01 -0500

    Submarine class outline

---
+++ core/src/com/mwojnar/GameObjects/Submarine.java
@@ -0,0 +1,31 @@
+package com.mwojnar.GameObjects;
+
+import java.util.List;
+
+import com.playgon.GameEngine.Entity;
+import com.playgon.GameEngine.TouchEvent;
+import com.playgon.GameWorld.GameWorld;
+
+public class Submarine extends Entity {
+
+	public Submarine(GameWorld myWorld) {
+		
+		super(myWorld);
+		
+	}
+	
+	@Override
+	public void update(float delta, List<TouchEvent> touchEventList, List<Character> charactersTyped, List<Integer> keysFirstDown, List<Integer> keysFirstUp, List<Integer> keysDown) {
+		
+		
+		
+	}
+	
+	@Override
+	public void draw(GameRenderer renderer) {
+		
+		
+		
+	}
+
+}
